0787a8d52f358f0763a23382b3398b200cb4ecdb
tylerryan45 <tylerryan2022@gmail.com>
2026-04-26 17:01:32 -0500

    Added xml support to saving plugin state, initialized folder structure and files for GUI

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -172,15 +172,17 @@ juce::AudioProcessorEditor* CompressorPluginAudioProcessor::createEditor()
 //==============================================================================
 void CompressorPluginAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    auto state = aptvs.copyState();
+    std::unique_ptr<juce::XmlElement> xml (state.createXml());
+    copyXmlToBinary (*xml, destData);
 }
 
 void CompressorPluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+    std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+        if (xmlState.get() != nullptr)
+            if (xmlState->hasTagName (aptvs.state.getType()))
+                aptvs.replaceState (juce::ValueTree::fromXml (*xmlState));
 }
 
 //==============================================================================
